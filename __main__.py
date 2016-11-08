dd9ade1a7a6930c924dea073b72a48f59b33ddb9
Richard G. Roman <rits@rits.hu>
2016-11-09 00:59:01 +0100

    Initial version

---
+++ __main__.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+import sys
+import time
+import traceback
+
+from controller import Controller
+
+def initLog(log_file, log_level):
+
+    # disable http request info logs:
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+    log_format = '%(asctime)s %(filename)s:%(lineno)s %(levelname)s %(message)s'
+    log_datefmt = '%Y-%m-%d %H:%M:%S'
+    logging.basicConfig(stream=sys.stdout,
+                        level=log_level,
+                        format=log_format,
+                        datefmt=log_datefmt)
+    return # TODO console-log configable
+    try:
+        logging.basicConfig(filename=log_file,
+                            level=log_level,
+                            format=log_format,
+                            datefmt=log_datefmt)
+    except OSError:  # we prepare for read-only filesystem here, fall back to console:
+        logging.basicConfig(stream=sys.stdout,
+                            level=log_level,
+                            format=log_format,
+                            datefmt=log_datefmt)
+
+def createController():
+    server = ''
+    port = 0
+    client_id = ''
+    email = ''
+    if len(sys.argv) == 4:
+        server = sys.argv[1]
+        port = sys.argv[2]
+        client_id = sys.argv[3]
+    elif len(sys.argv) == 3:
+        port = sys.argv[1]
+        email = sys.argv[2]
+    else:
+        print("Usage (client mode):")
+        print("    " + sys.argv[0] + " <server> <port> <clientid>")
+        print("Usage (server mode):")
+        print("    " + sys.argv[0] + " <port> <alert_email_address>")
+        sys.exit()
+
+    return Controller(server, port, email, client_id)
+
+
+if __name__ == '__main__':
+
+    log_file = "/tmp/serverheartbeat.log"
+    # -- see logging's source (DEBUG=10, INFO=20, WARNING=30, ERROR=40)
+    initLog(log_file, logging.DEBUG)
+
+    logging.info("--------- Init ---------")
+    c = createController()
+
+    firstrun = True
+    while True :
+        try:
+            if not firstrun:
+                time.sleep(30)
+            firstrun = False
+            c.run()
+        except KeyboardInterrupt:
+            logging.info("Exiting after KeyboardInterrupt")
+            sys.exit()
+        except SystemExit:
+            logging.info("Exiting after sys.exit()")
+            sys.exit()
+        except:
+            logging.error(traceback.format_exc())
+            print(traceback.format_exc())
+            logging.error("Unexpected error, waiting and restarting...")
+            print("Unexpected error, waiting and restarting...")
+            #logging.error("Unexpected error", sys.exc_info()[0])
+            #print("Unexpected error" + sys.exc_info()[0], file = sys.stderr)
+
---
+++ controller.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+import asyncio
+import logging
+import sys
+import time
+
+import smtplib
+from email.mime.text import MIMEText
+
+
+class Controller:
+    """Serves as factory, container and coordinator of Service classes"""
+
+    class ServerProtocol(asyncio.Protocol):
+        def __init__(self, controller):
+            self.controller = controller
+        
+        def connection_made(self, transport):
+            pass
+        
+        def connection_lost(self, exception):
+            pass
+
+        def datagram_received(self, data, addr):
+            logging.info('data from {}: {}'.format(addr, data))
+
+            fields = data.decode().split()
+            if fields[0] != "hb" or len(fields) < 2:
+                logging.error('wrong format, "hb <client_id>" expected')
+                return
+
+            client_id = fields[1]
+            self.controller.heartbeatReceived(client_id)
+
+    class ClientProtocol(asyncio.Protocol):
+        def __init__(self, controller):
+            controller.clientProtocol = self
+            self.transport = None
+
+        def connection_made(self, transport):
+            self.transport = transport
+            self.peername = self.transport.get_extra_info('peername')
+            logging.info('new connection to {}'.format(self.peername))
+
+        def write(self, msgStr):
+            if self.transport:
+                self.transport.sendto(msgStr.encode())
+
+    ### Controller ###
+    def __init__(self, server, port, email, client_id):
+        """ server: if None, we're the server process """
+        self.server = server
+        self.port = port
+        self.email = email
+        self.client_id = client_id
+        self.clientProtocol = None
+        self.lastSeenTimes = {}
+
+        if not self.server and not self.email:
+            logging.error("We need a valid email address in server mode")
+            print("We need a valid email address in server mode")
+            sys.exit()
+
+    def sendMail(self, client_id, upOrDown):
+        if not self.email:
+            return
+
+        msg = MIMEText("Thanks...")
+        msg['Subject'] = 'Client {} is {}'.format(client_id, upOrDown)
+        msg['From'] = "donotreply@rits.hu"
+        msg['To'] = self.email
+
+        # Send the message via our own SMTP server.
+        s = smtplib.SMTP('localhost')
+        s.send_message(msg)
+        s.quit()
+
+
+    def heartbeatReceived(self, client_id):
+        if not client_id in self.lastSeenTimes:
+            logging.info("Client {} is up.".format(client_id))
+            self.sendMail(client_id, "UP")
+        self.lastSeenTimes[client_id] = time.time()
+
+    def checkTimeouts(self):
+        now = time.time()
+        todelete = set()
+        for client_id in self.lastSeenTimes.keys():
+            if now - self.lastSeenTimes[client_id] > 4:
+                logging.info("Client {} is down.".format(client_id))
+                self.sendMail(client_id, "DOWN")
+                todelete.add(client_id)
+
+        for client_id in todelete:
+            del self.lastSeenTimes[client_id]
+
+        self.eventloop.call_later(1, self.checkTimeouts)
+
+    def sendHeartbeat(self):
+        if self.clientProtocol:
+            self.clientProtocol.write("hb {}\r\n".format(self.client_id))
+        self.eventloop.call_later(3, self.sendHeartbeat)
+
+    def run(self):
+        logging.info("Controller startup")
+        self.eventloop = asyncio.get_event_loop()
+
+        # eventloop.set_debug(True)
+
+        if self.server:
+            logging.info("Starting as client")
+            coroutine = self.eventloop.create_datagram_endpoint(lambda: Controller.ClientProtocol(self), remote_addr=(self.server, self.port))
+            self.eventloop.call_later(3, self.sendHeartbeat)
+        else:
+            logging.info("Starting as server")
+            coroutine = self.eventloop.create_datagram_endpoint(lambda: Controller.ServerProtocol(self), local_addr=('127.0.0.1', self.port))
+            self.eventloop.call_later(4, self.checkTimeouts)
+
+        asyncio.ensure_future(coroutine, loop=self.eventloop)  # starts connection in the background
+
+
+        self.eventloop.run_forever()
+
+
